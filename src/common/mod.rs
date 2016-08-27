a294b8c17465dd1d0e618766a957e2a48a41fb30
LambdaXymox <lambda.xymox@gmail.com>
2016-08-26 23:29:58 -0500

    Refactor redundant simple data structure code into a macro.

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -11,7 +11,7 @@ pub use self::i2p_integer::I2pInt56;
 pub use self::i2p_integer::I2pInt64;
 pub use self::public_key::PublicKey;
 pub use self::private_key::PrivateKey;
-pub use self::i2p_hash::Hash256;
+//pub use self::i2p_hash::Hash256;
 pub use self::session_key::SessionKey;
 pub use self::session_tag::SessionTag;
 pub use self::signature::SigningPublicKey;
@@ -22,9 +22,14 @@ pub use self::signature::Signature;
 mod i2p_integer;
 mod i2p_date;
 mod i2p_string;
+
+#[macro_use]
+mod simple_data_structure;
+
 mod i2p_hash;
 mod public_key;
 mod private_key;
 mod session_key;
 mod session_tag;
+
 mod signature;
---
+++ src/common/simple_data_structure.rs
@@ -0,0 +1,144 @@
+use std::fmt;
+use std::fmt::Write;
+use rustc_serialize::base64::ToBase64;
+use rustc_serialize::base64;
+
+
+macro_rules! simple_data_structure {
+    ($TYPE_NAME:ident, $ARRAY_LENGTH:expr) => {
+        #[derive(Copy, Eq)]
+        pub struct $TYPE_NAME {
+            data: [u8; $ARRAY_LENGTH]
+        }
+
+        impl $TYPE_NAME {
+            fn new(data: [u8; $ARRAY_LENGTH]) -> $TYPE_NAME {
+                $TYPE_NAME {
+                    data: data
+                }
+            }
+
+            /// Returns the length of an `$TYPE_NAME` in bytes.
+            pub fn len(&self) -> usize {
+                $ARRAY_LENGTH
+            }
+
+            fn from_slice(slice: &[u8]) -> Option<$TYPE_NAME> {
+                if slice.len() <= $ARRAY_LENGTH {
+                    let mut key_bytes = [0x00; $ARRAY_LENGTH];
+                    let offset = $ARRAY_LENGTH - slice.len();
+                    for i in 0..slice.len() {
+                        key_bytes[i + offset] = slice[i];
+                    }
+                    Some($TYPE_NAME::new(key_bytes))
+                } else {
+                    None
+                }
+            }
+
+            fn as_slice(&self) -> &[u8] {
+                self.data.as_ref()
+            }
+        }
+
+        impl Default for $TYPE_NAME {
+            fn default() -> $TYPE_NAME {
+                $TYPE_NAME::new([0x00; $ARRAY_LENGTH])
+            }
+        }
+
+        impl Clone for $TYPE_NAME {
+            fn clone(&self) -> $TYPE_NAME {
+                let mut cloned_hash = [0x00; $ARRAY_LENGTH];
+                for i in 0..self.len() {
+                    cloned_hash[i] = self.data[i];
+                }
+
+                $TYPE_NAME::new(cloned_hash)
+            }
+        }
+
+        impl PartialEq for $TYPE_NAME {
+            fn eq(&self, other: &$TYPE_NAME) -> bool {
+                for i in 0..self.len() {
+                    if self.data[i] != other.data[i] {
+                        return false;
+                    }
+                }
+
+                true
+            }
+        }
+
+        impl From<[u8; $ARRAY_LENGTH]> for $TYPE_NAME {
+            fn from(data: [u8; $ARRAY_LENGTH]) -> $TYPE_NAME {
+                $TYPE_NAME::new(data)
+            }
+        }
+
+        impl<'a> From<&'a [u8; $ARRAY_LENGTH]> for $TYPE_NAME {
+            fn from(data: &'a [u8; $ARRAY_LENGTH]) -> $TYPE_NAME {
+                let mut cloned_data = [0x00; $ARRAY_LENGTH];
+                for i in 0..data.len() {
+                    cloned_data[i] = data[i];
+                }
+
+                $TYPE_NAME::new(cloned_data)
+            }
+        }
+
+        impl fmt::Display for $TYPE_NAME {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                fn config() -> base64::Config {
+                    base64::Config {
+                        char_set: base64::CharacterSet::Standard,
+                        newline: base64::Newline::LF,
+                        pad: false,
+                        line_length: None
+                    }
+                }
+
+                write!(f, "{}", self.as_slice().to_base64(config()))
+            }
+        }
+
+        impl fmt::LowerHex for $TYPE_NAME {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                let mut output = String::new();
+                for byte in self.as_ref() {
+                    write!(output, "{:02x}", byte);
+                }
+
+                write!(f, "{}", output)
+            }
+        }
+
+        impl fmt::UpperHex for $TYPE_NAME {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                let mut output = String::new();
+                for byte in self.as_ref() {
+                    write!(output, "{:02X}", byte);
+                }
+
+                write!(f, "{}", output)
+            }
+        }
+
+        impl fmt::Binary for $TYPE_NAME {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                let mut output = String::new();
+                for byte in self.as_ref() {
+                    write!(output, "{:08b}", byte);
+                }
+
+                write!(f, "{}", output)
+            }
+        }
+
+        impl AsRef<[u8]> for $TYPE_NAME {
+            fn as_ref(&self) -> &[u8] {
+                self.as_slice()
+            }
+        }
+    }
+}
