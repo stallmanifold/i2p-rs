b7a9395c5b630382cbf8c1e9975a902a9ae222f9
LambdaXymox <lambda.xymox@gmail.com>
2016-08-31 05:45:03 -0500

    Bump version number.

--- src/tests/common/i2p_string.rs
+++ src/tests/common/i2p_string.rs
@@ -52,14 +52,14 @@ mod property_tests {
     }
 }
 
-mod serialization_tests {
+mod serialization_property_tests {
     use common::I2pString;
     use common;
     use quickcheck;
     use serialize::{Serialize, Deserialize};
 
 
-    const BUFFER_LENGTH: usize = 256;
+    const PROP_BUFFER_LENGTH: usize = 256;
 
     fn equal(slice1: &[u8], slice2: &[u8]) -> bool {
         if slice1.len() == slice2.len() {
@@ -78,7 +78,7 @@ mod serialization_tests {
     #[test]
     fn prop_i2p_string_serialize_deserialize_output_should_be_equal_to_input() {
         fn property(string: I2pString) -> bool {
-            let mut serialized_string = [0x00; BUFFER_LENGTH];
+            let mut serialized_string = [0x00; PROP_BUFFER_LENGTH];
             Serialize::serialize(&string, serialized_string.as_mut()).unwrap();
             let deserialized_string = <I2pString as Deserialize>::deserialize(serialized_string.as_ref()).unwrap();
 
@@ -90,7 +90,7 @@ mod serialization_tests {
     #[test]
     fn prop_i2p_string_serialize_deserialize_output_should_have_equal_length_to_input() {
         fn property(string: I2pString) -> bool {
-            let mut serialized_string = [0x00; BUFFER_LENGTH];
+            let mut serialized_string = [0x00; PROP_BUFFER_LENGTH];
             Serialize::serialize(&string, serialized_string.as_mut()).unwrap();
             let deserialized_string = <I2pString as Deserialize>::deserialize(serialized_string.as_ref()).unwrap();
 
@@ -102,11 +102,11 @@ mod serialization_tests {
     #[test]
     fn prop_i2p_string_deserialize_serialize_output_should_be_equal_to_input() {
         fn property(string: I2pString) -> bool {
-            let mut serialized_string = [0x00; BUFFER_LENGTH];
+            let mut serialized_string = [0x00; PROP_BUFFER_LENGTH];
             Serialize::serialize(&string, serialized_string.as_mut()).unwrap();
             let deserialized_string = <I2pString as Deserialize>::deserialize(serialized_string.as_ref()).unwrap();
 
-            let mut reserialized_string = [0x00; BUFFER_LENGTH];
+            let mut reserialized_string = [0x00; PROP_BUFFER_LENGTH];
             Serialize::serialize(&deserialized_string, reserialized_string.as_mut()).unwrap();
 
             // Check that the serialization and the reserialization match bytewise.
@@ -115,21 +115,6 @@ mod serialization_tests {
         quickcheck::quickcheck(property as fn(I2pString) -> bool);
     }
 
-    #[test]
-    fn test_serialize_should_fail_without_writing_to_buffer_when_buffer_too_small() {
-        let raw_string_bytes: [u8; BUFFER_LENGTH-1] = [b'A'; BUFFER_LENGTH-1];
-        let string = I2pString::from_utf8(raw_string_bytes.as_ref()).unwrap();
-        let mut buffer: [u8; 100] = [0x00; 100];
-        assert!(BUFFER_LENGTH > 100);
-
-        let result = Serialize::serialize(&string, buffer.as_mut());
-        assert!(result.is_err());
-
-        for byte in buffer.iter() {
-            assert_eq!(*byte, 0x00);
-        }
-
-    }
 
     #[test]
     fn prop_serialize_should_fail_without_writing_to_buffer_when_buffer_too_small() {
@@ -150,14 +135,14 @@ mod serialization_tests {
     #[test]
     fn prop_i2p_string_encoded_length_should_not_exceed_max_string_length() {
         fn property(string: I2pString) -> bool {
-            assert!(BUFFER_LENGTH < 300);
+            assert!(PROP_BUFFER_LENGTH < 300);
             let mut serialized_string = [0x00; 300];
             let bytes_written = match Serialize::serialize(&string, serialized_string.as_mut()) {
                 Ok(bytes) => bytes,
                 Err(_) => unreachable!()
             };
 
-            assert!(bytes_written <= BUFFER_LENGTH);
+            assert!(bytes_written <= PROP_BUFFER_LENGTH);
             assert_eq!(bytes_written, string.len()+1);
             for byte in serialized_string.iter().skip(bytes_written) {
                 if *byte != 0x00 {
@@ -167,5 +152,29 @@ mod serialization_tests {
             true
         }
         quickcheck::quickcheck(property as fn(I2pString) -> bool);
+    }
+}
+
+mod serialization_tests {
+    use common::I2pString;
+    use common;
+    use serialize::{Serialize, Deserialize};
+
+
+    const TEST_BUFFER_LENGTH: usize = 256;
+
+    #[test]
+    fn test_serialize_should_fail_without_writing_to_buffer_when_buffer_too_small() {
+        let raw_string_bytes: [u8; TEST_BUFFER_LENGTH-1] = [b'A'; TEST_BUFFER_LENGTH-1];
+        let string = I2pString::from_utf8(raw_string_bytes.as_ref()).unwrap();
+        let mut buffer: [u8; 100] = [0x00; 100];
+        assert!(TEST_BUFFER_LENGTH > 100);
+
+        let result = Serialize::serialize(&string, buffer.as_mut());
+        assert!(result.is_err());
+
+        for byte in buffer.iter() {
+            assert_eq!(*byte, 0x00);
+        }
     }
 }
